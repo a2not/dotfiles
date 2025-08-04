f5a0db2513eb795e2fbfdbf22e3a783cb568fcda
a2not <a2not.dev@gmail.com>
2025-08-04 13:41:28 +0900

    remove omz
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -1,25 +1,14 @@
 {...}: {
   programs.zsh = {
     enable = true;
-    autosuggestion.enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
     shellAliases = {
       l = "eza -lah --icons";
       ll = "eza -lah --icons";
       ls = "eza --icons";
     };
-
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "zsh-autosuggestions"
-        "zsh-syntax-highlighting"
-      ];
-      extraConfig = ''
-        zstyle ':omz:update' mode auto
-        zstyle ':omz:update' verbose minimal
-      '';
-    };
+    history.size = 100000;
   };
 }
