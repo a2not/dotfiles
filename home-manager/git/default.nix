21da8913f894677a897290023f8cfea49481992e
a2not <31874975+a2not@users.noreply.github.com>
2025-09-27 15:54:32 +0900

    fix(git): correct mac config path and update gpg settings
    - Fix typo in `includeIf` path for macOS in `.gitconfig`
    - Remove redundant user signing key and gpg settings from `.gitconfig_mac`
    - Ensure `.gitconfig_mac` is managed by home-manager
    
    These changes improve compatibility and centralize GPG SSH signing
    configuration for macOS environments.
    
    Signed-off-by: a2not <31874975+a2not@users.noreply.github.com>

--- home-manager/git/default.nix
+++ home-manager/git/default.nix
@@ -10,6 +10,7 @@
   home.file = {
     ".gitconfig".source = ./.gitconfig;
     ".gitconfig_work".source = ./.gitconfig_work;
+    ".gitconfig_mac".source = ./.gitconfig_mac;
     ".ssh/allowed_signers".source = ./allowed_signers;
   };
 }
