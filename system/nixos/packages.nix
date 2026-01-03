3275824e4a3991ca275805595271f8223e7b4f9b
a2not <31874975+a2not@users.noreply.github.com>
2026-01-03 09:54:06 +0900

    feat(nixos): enable nix-ld and set zsh as default shell
    Set zsh as the default user shell and enable nix-ld with required
    libraries for better compatibility with prebuilt binaries. Also enable
    Docker and related tools for container management.
    
    Signed-off-by: a2not <31874975+a2not@users.noreply.github.com>

--- system/nixos/packages.nix
+++ system/nixos/packages.nix
@@ -5,19 +5,24 @@
     gnumake
   ];
 
+  # set zsh as default shell
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [zsh];
 
+  # enable docker
   virtualisation.docker.enable = true;
-
   virtualisation.docker.extraPackages = with pkgs; [
     docker-buildx
     docker-compose
   ];
 
-  # TODO: nix-ld
-  environment.sessionVariables = {
-    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  # enable nix-ld, i feel defeated i know...
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      stdenv.cc.cc.lib
+      zlib
+    ];
   };
 }
