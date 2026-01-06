34f4b323d33daf170af2560cd5a5c32e74d97f4b
a2not <31874975+a2not@users.noreply.github.com>
2026-01-06 11:57:07 +0900

    feat(treesitter): update to main branch and improve config
    Switch nvim-treesitter and textobjects to the main branch for better
    compatibility. Remove unnecessary lazy loading and event/init logic to
    simplify setup. Update dependencies and configuration for improved
    performance and reliability.
    
    Signed-off-by: a2not <31874975+a2not@users.noreply.github.com>

--- home-manager/neovim/nvim/lua/plugins/treesitter.lua
+++ home-manager/neovim/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,15 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'main',
+    lazy = false,
     build = ':TSUpdate',
-    event = { 'BufEnter' },
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
-      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require('lazy.core.loader').add_to_rtp(plugin)
-      require('nvim-treesitter.query_predicates')
-    end,
     dependencies = {
       'nvim-treesitter/nvim-treesitter-context',
-      'nvim-treesitter/nvim-treesitter-textobjects',
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        branch = 'main',
+      },
       {
         'm-demare/hlargs.nvim',
         config = true,
@@ -61,7 +56,6 @@ return {
         },
       },
     },
-    cmd = { 'TSUpdateSync', 'TSUpdate', 'TSInstall' },
 
     opts = {
       ensure_installed = {
