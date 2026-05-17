bb6aef9d571750987fa4dfd91164373405a0552c
a2not <a2not.dev@gmail.com>
2026-05-17 19:06:20 +0900

    feat(tmux): switch to minimal-tmux-status plugin
    Replace the tokyo-night-tmux plugin and its configuration with the
    minimal-tmux-status plugin for a simpler tmux status line. This change
    removes custom theme and icon settings in favor of a minimal appearance.
    
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -3,17 +3,7 @@
     enable = true;
     extraConfig = builtins.readFile ./tmux.conf;
     plugins = with pkgs; [
-      {
-        plugin = tmuxPlugins.tokyo-night-tmux;
-        extraConfig = ''
-          set -g @tokyo-night-tmux_theme night
-          set -g @tokyo-night-tmux_transparent 1
-          set -g @tokyo-night-tmux_window_id_style none
-          set -g @tokyo-night-tmux_pane_id_style hide
-          set -g @tokyo-night-tmux_zoom_id_style hide
-          set -g @tokyo-night-tmux_window_tidy_icons 1
-        '';
-      }
+      tmuxPlugins.minimal-tmux-status
     ];
   };
 
