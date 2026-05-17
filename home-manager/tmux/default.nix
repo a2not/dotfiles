32caaca0e9048f47f456df11dc87b3d802dbddef
a2not <a2not.dev@gmail.com>
2026-05-17 18:53:19 +0900

    feat(tmux): customize tokyo-night-tmux plugin settings
    Add custom configuration for the tokyo-night-tmux plugin, including theme
    selection, transparency, and style options for window and pane IDs. This
    improves the tmux appearance and usability.
    
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- home-manager/tmux/default.nix
+++ home-manager/tmux/default.nix
@@ -3,7 +3,17 @@
     enable = true;
     extraConfig = builtins.readFile ./tmux.conf;
     plugins = with pkgs; [
-      tmuxPlugins.tokyo-night-tmux
+      {
+        plugin = tmuxPlugins.tokyo-night-tmux;
+        extraConfig = ''
+          set -g @tokyo-night-tmux_theme night
+          set -g @tokyo-night-tmux_transparent 1
+          set -g @tokyo-night-tmux_window_id_style none
+          set -g @tokyo-night-tmux_pane_id_style hide
+          set -g @tokyo-night-tmux_zoom_id_style hide
+          set -g @tokyo-night-tmux_window_tidy_icons 1
+        '';
+      }
     ];
   };
 
