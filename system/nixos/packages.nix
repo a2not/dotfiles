dab05793f3a904f93a83a6f238712ac86d1034c2
a2not <a2not.dev@gmail.com>
2026-05-19 23:50:24 +0900

    feat(nixos): add python build deps and CPython env vars
    Add essential build dependencies for python-build/pyenv to the NixOS
    packages list, including curl and pkg-config. Configure nix-ld and
    environment variables (CPATH, LIBRARY_PATH, PKG_CONFIG_PATH) to help
    CPython's configure script locate headers and libraries on NixOS,
    enabling successful source builds of Python and related tooling.
    
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- system/nixos/packages.nix
+++ system/nixos/packages.nix
@@ -3,6 +3,10 @@
     vim
     git
     gnumake
+
+    # python-build / pyenv build dependencies
+    curl # downloader used by python-build
+    pkg-config # CPython configure uses pkg-config to locate libs
   ];
 
   # set zsh as default shell
@@ -17,12 +21,75 @@
     docker-compose
   ];
 
-  # enable nix-ld, i feel defeated i know...
+  # enable nix-ld (for non-Nix binaries to find runtime libs)
   programs.nix-ld = {
     enable = true;
     libraries = with pkgs; [
       stdenv.cc.cc.lib
       zlib
+
+      zlib.dev
+      openssl.dev
+      readline.dev
+      bzip2.dev
+      libffi.dev
+      sqlite.dev
+      xz.dev
+      ncurses.dev
+      zstd.dev
+      tk.dev
+      gdbm.dev
+      libuuid.dev
+      expat.dev
     ];
+  };
+
+  # Tell CPython's ./configure where to find headers/libs on NixOS
+  # (there is no global /usr/include or /usr/lib)
+  environment.variables = {
+    # GCC header search path for build-time compilation
+    CPATH = with pkgs;
+      lib.makeSearchPathOutput "dev" "include" [
+        zlib.dev
+        openssl.dev
+        readline.dev
+        bzip2.dev
+        libffi.dev
+        sqlite.dev
+        xz.dev
+        ncurses.dev
+        zstd.dev
+        tk.dev
+        gdbm.dev
+        libuuid.dev
+        expat.dev
+      ];
+
+    # GCC library search path for link-time
+    LIBRARY_PATH = with pkgs;
+      lib.makeSearchPath "lib" [
+        zlib
+        readline
+        libffi
+        ncurses
+        tk
+        tcl
+        expat
+      ];
+
+    # pkg-config search path so ./configure auto-discovers deps
+    PKG_CONFIG_PATH = with pkgs;
+      lib.makeSearchPathOutput "dev" "lib/pkgconfig" [
+        openssl
+        readline
+        bzip2
+        libffi
+        sqlite
+        xz
+        ncurses
+        zstd
+        libuuid
+        expat
+      ];
   };
 }
