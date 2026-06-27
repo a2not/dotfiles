0a0cd437085e6389faf249a22be787914bbd0cbc
a2not <a2not.dev@gmail.com>
2026-06-27 12:44:39 +0900

    feat(zsh): add pi-guardrails config
    Add pi-guardrails extension configuration and register it in the pi
    agent settings. Introduce home-manager/zsh/pi/agent/extensions/
    guardrails.json with the extension schema and a permission gate rule
    to block destructive "rm .*" commands. Expose the file from the
    dotfiles tree via config.lib.file.mkOutOfStoreSymlink in default.nix
    and document avoiding cloning the repo because some extensions need
    writable node_modules while /nix/store is read-only.
    
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- home-manager/zsh/default.nix
+++ home-manager/zsh/default.nix
@@ -125,6 +125,9 @@ in {
     # "git:github.com/DietrichGebert/ponytail@025da371cd7539c3eb0ad859b08b3ca55e695f16", # https://github.com/DietrichGebert/ponytail/releases/tag/v4.8.3
     # "git:github.com/nicobailon/pi-subagents@e4f06282d0c95856b36b7ec2893f4fd294ebfefe", # https://github.com/nicobailon/pi-subagents/releases/tag/v0.31.0
     # "git:github.com/nicobailon/pi-web-access@7bdc30a65cf77273eb9c0034647b373bda4060d7" # https://github.com/nicobailon/pi-web-access/releases/tag/v0.13.0
+    # "git:github.com/aliou/pi-guardrails@79f61e5a3f85484931487db0ed65887e96afc436"      # https://github.com/aliou/pi-guardrails/releases/tag/v0.15.0
+    # NOTE: currently avoiding cloning them by flake.nix since some of them needs local node_modules writable but `/nix/store` is read only.
+    ".pi/agent/extensions/guardrails.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home-manager/zsh/pi/agent/extensions/guardrails.json";
   };
 
   sops = {
