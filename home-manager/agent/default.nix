942d922c8e7aec8351eee659df422efdb1db6438
a2not <a2not.dev@gmail.com>
2026-06-29 23:16:18 +0900

    restored superpower
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- home-manager/agent/default.nix
+++ home-manager/agent/default.nix
@@ -37,6 +37,7 @@
     # current extensions: (since settings.json can't have inline comment)
     # "git:github.com/mksglu/context-mode@bbeedad88550b09e77b09e6622689be7b5616c51",     # https://github.com/mksglu/context-mode/releases/tag/v1.0.167
     # "git:github.com/DietrichGebert/ponytail@025da371cd7539c3eb0ad859b08b3ca55e695f16", # https://github.com/DietrichGebert/ponytail/releases/tag/v4.8.3
+    # "git:github.com/obra/superpowers@896224c4b1879920ab573417e68fd51d2ccc9072",        # https://github.com/obra/superpowers/releases/tag/v6.0.3
     # "git:github.com/nicobailon/pi-subagents@e4f06282d0c95856b36b7ec2893f4fd294ebfefe", # https://github.com/nicobailon/pi-subagents/releases/tag/v0.31.0
     # "git:github.com/nicobailon/pi-web-access@7bdc30a65cf77273eb9c0034647b373bda4060d7" # https://github.com/nicobailon/pi-web-access/releases/tag/v0.13.0
     # "git:github.com/aliou/pi-guardrails@79f61e5a3f85484931487db0ed65887e96afc436"      # https://github.com/aliou/pi-guardrails/releases/tag/v0.15.0
