520af5f1bd9f54e73a8ceed8090fb0383deda49a
a2not <a2not.dev@gmail.com>
2026-08-21 13:53:03 +0900

    chore(agent): bump package pins; add ssrf setting
    Update pinned package commits for ponytail, pi-subagents, pi-web-access,
    and pi-guardrails to their latest tested commits.
    
    Also enable trustEnvProxy in web-search.json to respect environment
    proxy settings for SSRF handling.
    
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- home-manager/agent/default.nix
+++ home-manager/agent/default.nix
@@ -31,11 +31,10 @@ in {
     ".pi/agent/settings.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home-manager/agent/pi/agent/settings.json";
     # NOTE: for updating extensions, check for the latest tagged version and pin by commit hash.
     # current extensions: (since settings.json can't have inline comment)
-    # "git:github.com/DietrichGebert/ponytail@025da371cd7539c3eb0ad859b08b3ca55e695f16", # https://github.com/DietrichGebert/ponytail/releases/tag/v4.8.3
-    # "git:github.com/obra/superpowers@896224c4b1879920ab573417e68fd51d2ccc9072",        # https://github.com/obra/superpowers/releases/tag/v6.0.3
-    # "git:github.com/nicobailon/pi-subagents@e4f06282d0c95856b36b7ec2893f4fd294ebfefe", # https://github.com/nicobailon/pi-subagents/releases/tag/v0.31.0
-    # "git:github.com/nicobailon/pi-web-access@7bdc30a65cf77273eb9c0034647b373bda4060d7" # https://github.com/nicobailon/pi-web-access/releases/tag/v0.13.0
-    # "git:github.com/aliou/pi-guardrails@79f61e5a3f85484931487db0ed65887e96afc436"      # https://github.com/aliou/pi-guardrails/releases/tag/v0.15.0
+    # "git:github.com/DietrichGebert/ponytail@0a4dd63ad4541f4f655c4108a295916f3c1d8fda", # https://github.com/DietrichGebert/ponytail/releases/tag/v4.9.0
+    # "git:github.com/nicobailon/pi-subagents@c91f4de5ea956f0d1fcab9e44eba079fa2f917dd", # https://github.com/nicobailon/pi-subagents/releases/tag/v0.53.0
+    # "git:github.com/nicobailon/pi-web-access@3b875f574840eebae39e5fede0d99a5f7c71f482" # https://github.com/nicobailon/pi-web-access/releases/tag/v0.24.0
+    # "git:github.com/aliou/pi-guardrails@a3da058432e33999ed368b7a2ec5c72cc2187de2"      # https://github.com/aliou/pi-guardrails/releases/tag/v0.17.0
     # NOTE: currently avoiding cloning them by flake.nix since some of them needs local node_modules writable but `/nix/store` is read only.
     ".pi/agent/extensions/guardrails.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home-manager/agent/pi/agent/extensions/guardrails.json";
     ".pi/web-search.json".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/home-manager/agent/pi/web-search.json";
