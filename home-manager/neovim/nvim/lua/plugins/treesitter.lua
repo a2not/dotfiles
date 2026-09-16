7e7c61efd0e29c4fe8b8b4373cdc97383a04f5b6
a2not <a2not.dev@gmail.com>
2026-09-16 16:08:54 +0900

    feat(treesitter): add textobject motions
    Add move and select keymaps options to nvim-treesitter-textobjects
    plugin, enabling motions (goto next/previous start/end) and textobject
    selections for functions, classes, blocks, loops, comments, and
    parameters.
    
    Update lazy-lock.json to bump several plugin commits to their latest
    versions.
    
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- home-manager/neovim/nvim/lua/plugins/treesitter.lua
+++ home-manager/neovim/nvim/lua/plugins/treesitter.lua
@@ -66,6 +66,54 @@ return {
     'nvim-treesitter/nvim-treesitter-textobjects',
     branch = 'main',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
+    opts = {
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+          [']b'] = '@block.outer',
+          [']a'] = '@parameter.inner',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+          [']B'] = '@block.outer',
+          [']A'] = '@parameter.inner',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+          ['[b'] = '@block.outer',
+          ['[a'] = '@parameter.inner',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+          ['[B'] = '@block.outer',
+          ['[A'] = '@parameter.inner',
+        },
+      },
+      select = {
+        enable = true,
+        lookahead = true,
+        keymaps = {
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+          ['ab'] = '@block.outer',
+          ['ib'] = '@block.inner',
+          ['al'] = '@loop.outer',
+          ['il'] = '@loop.inner',
+          ['a/'] = '@comment.outer',
+          ['i/'] = '@comment.outer', -- no inner for comment
+          ['aa'] = '@parameter.outer', -- parameter -> argument
+          ['ia'] = '@parameter.inner',
+        },
+      },
+    },
   },
 
   {
