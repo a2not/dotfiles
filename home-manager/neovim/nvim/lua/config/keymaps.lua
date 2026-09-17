f0bb6c216f33cf014651f09f9bcbaf9a0ce95470
a2not <a2not.dev@gmail.com>
2026-09-17 10:45:42 +0900

    chore(keymaps): remove center-buffer mappings
    Remove center-buffer mappings that appended 'zz' to many normal-mode
    motions.
    
    These mappings changed default navigation behavior by centering the cursor
    after motions like <C-u>/<C-d>/n/N/G/gg/%/*/# and jumps (<C-i>/<C-o>).
    Removing them restores default motion semantics and avoids unexpected
    viewport movement.
    
    Signed-off-by: a2not <a2not.dev@gmail.com>

--- home-manager/neovim/nvim/lua/config/keymaps.lua
+++ home-manager/neovim/nvim/lua/config/keymaps.lua
@@ -22,21 +22,6 @@ vim.keymap.set(
 vim.keymap.set('v', 'J', [[:m '>+1<CR>gv=gv]]) -- move selected line down
 vim.keymap.set('v', 'K', [[:m '<-2<CR>gv=gv]]) -- move selected line up
 
--- Center buffer while navigating
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
-vim.keymap.set('n', '{', '{zz', { noremap = true, silent = true })
-vim.keymap.set('n', '}', '}zz', { noremap = true, silent = true })
-vim.keymap.set('n', 'N', 'Nzz', { noremap = true, silent = true })
-vim.keymap.set('n', 'n', 'nzz', { noremap = true, silent = true })
-vim.keymap.set('n', 'G', 'Gzz', { noremap = true, silent = true })
-vim.keymap.set('n', 'gg', 'ggzz', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-i>', '<C-i>zz', { noremap = true, silent = true })
-vim.keymap.set('n', '<C-o>', '<C-o>zz', { noremap = true, silent = true })
-vim.keymap.set('n', '%', '%zz', { noremap = true, silent = true })
-vim.keymap.set('n', '*', '*zz', { noremap = true, silent = true })
-vim.keymap.set('n', '#', '#zz', { noremap = true, silent = true })
-
 -- quickfix list
 vim.keymap.set('n', '<leader>q', function()
   -- cclose if open, return immediately
